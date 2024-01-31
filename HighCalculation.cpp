721991ac7c740b7aa67f2ce38809c5458dc123a2
Kevin-467 <138954516+Kevin-467@users.noreply.github.com>
2024-01-31 19:53:09 +0800

    Add files via upload

---
+++ HighCalculation.cpp
@@ -0,0 +1,76 @@
+#include "HighCalculation.h"
+#include <algorithm>
+#include <vector>
+#include <iterator>
+#include <string>
+#include <cmath>
+
+HighCalculation HighCalculation::operator+(HighCalculation h1)
+{
+    return HighCalculation();
+}
+
+HighCalculation HighCalculation::operator-(HighCalculation h1)
+{
+    return HighCalculation();
+}
+
+HighCalculation HighCalculation::operator*(ull h1)
+{
+    return HighCalculation();
+}
+
+HighCalculation HighCalculation::operator*(HighCalculation h1)
+{
+    return HighCalculation();
+}
+
+HighCalculation HighCalculation::operator/(ull h1)
+{
+    return HighCalculation();
+}
+
+HighCalculation HighCalculation::operator/(HighCalculation h1)
+{
+    return HighCalculation();
+}
+
+HighCalculation HighCalculation::operator%(ull h1)
+{
+    return HighCalculation();
+}
+
+HighCalculation HighCalculation::operator%(HighCalculation h1)
+{
+    return HighCalculation();
+}
+
+HighCalculation HighCalculation::operator!()
+{
+    return HighCalculation();
+}
+
+bool HighCalculation::operator>(HighCalculation h1)
+{
+    return true;
+}
+
+bool HighCalculation::operator==(HighCalculation h1)
+{
+    return true;
+}
+
+bool operator<(HighCalculation h1, HighCalculation h2)
+{
+    return false;
+}
+
+bool operator>=(HighCalculation h1, HighCalculation h2)
+{
+    return false;
+}
+
+bool operator<=(HighCalculation h1, HighCalculation h2)
+{
+    return false;
+}
